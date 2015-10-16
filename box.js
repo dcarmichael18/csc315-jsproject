9e8a7835ddbdbdf8474eefe2f88573d9df7de2ab
dcarmichael18 <dcarmichael18@cornellcollege.edu>
2015-10-16 09:58:33 -0500

    fixed box.js and added more to the html-based path

--- box.js
+++ box.js
@@ -9,8 +9,10 @@ if (choice === "2") {
 	confirm("You are not about the 'getting sucked into mysterious portals' life. You run away.");
 	confirm("The vortex pulls the entire building in, and you have Bob arrested for attempted murder.");
 	confirm("While filling out paperwork, the planet explodes.");
-	window.location.assign.("gameover.htm");
+	window.location.assign("gameover.htm");
 }
 else {
 	confirm("You can't resist it. You let yourself be pulled into the vortex.");
+	confirm("You become an all-powerful god, remaking the world as you see fit.");
+	window.location.assign("win.htm");
 }
