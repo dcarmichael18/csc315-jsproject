7dc6d6f0584432047395112f8fd36a1cea637478
dcarmichael18 <dcarmichael18@cornellcollege.edu>
2015-10-19 10:54:36 -0500

    updated index and changed links

--- read-more.js
+++ read-more.js
@@ -1,9 +1,10 @@
 function showHide(shID) {
    if (document.getElementById(shID)) {
+      //what to do when clicking to show
       if (document.getElementById(shID+'-show').style.display != 'none') {
-        // document.getElementById(shID+'-show').style.display = 'none';
          document.getElementById(shID).style.display = 'block';
       }
+      //what to do when clicking to hide
       else {
          document.getElementById(shID+'-show').style.display = 'inline';
          document.getElementById(shID).style.display = 'none';
