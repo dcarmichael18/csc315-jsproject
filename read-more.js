aae989e4e584858cc53a48429eef45f41371e3ff
dcarmichael18 <dcarmichael18@cornellcollege.edu>
2015-10-19 10:16:43 -0500

    worked on making the stylesheet and javascript code load

--- read-more.js
+++ read-more.js
@@ -1,7 +1,7 @@
 function showHide(shID) {
    if (document.getElementById(shID)) {
       if (document.getElementById(shID+'-show').style.display != 'none') {
-         document.getElementById(shID+'-show').style.display = 'none';
+        // document.getElementById(shID+'-show').style.display = 'none';
          document.getElementById(shID).style.display = 'block';
       }
       else {
